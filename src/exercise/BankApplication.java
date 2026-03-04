67d69372203450ff08af271dc0c0054268df611a
studioym-bb <studioym.bb@gmail.com>
2026-03-04 20:38:25 +0900

    day 6 laaast update night

--- src/exercise/BankApplication.java
+++ src/exercise/BankApplication.java
@@ -7,6 +7,13 @@ public class BankApplication {
 	public static void main(String[] args) {
 		Scanner in = new Scanner(System.in);
 
+		Account[] account = new Account[100];
+
+		String acN = "";
+		String acA = "";
+		int amount = 0;
+		int count = 0;
+
 		String template = ("""
 				-----------------------------------------------
 				1. 계좌생성 | 2. 계좌목록 | 3. 예금 | 4. 출금 | 5. 종료
@@ -21,31 +28,68 @@ public class BankApplication {
 			if (menu.equals("5"))
 				break;
 			switch (menu) {
-			case "1" -> createAccount(); // ==> 함수로 리팩토링
-			case "2" -> accountInquiry();
-			case "3" -> depositProcess();
-			case "4" -> withdrawProcess();
+			case "1" -> { // 계좌 개설
+				System.out.println("계좌 생성 처리 중...\n");
+				System.out.println("계좌 번호를 입력해주세요. -> ");
+				acN = in.nextLine();
+				System.out.println("계좌주 명을 입력해주세요. -> ");
+				acA = in.nextLine();
+				System.out.println("입금액을 입력해주세요. -> ");
+				amount = Integer.parseInt(in.nextLine());
+
+				Account account1 = new Account(acN, acA, amount);
+
+				account[count] = account1;
+				count++;
+			}
+			case "2" -> { // 계좌 목록 조회
+				System.out.println("계좌 목록 조회 중...\n");
+				for (int i = 0; i < count; i++) {
+					System.out.println("계좌번호 : " + account[i].getAccountNo() + "\n계좌주 : " + account[i].getAccountOwner()
+							+ "\n잔액 " + account[i].getBalance() + "\n----------------");
+				}
+			}
+			case "3" -> { // 예금
+				System.out.println("예금 처리중... \n");
+				System.out.println("계좌번호를 입력해주세요. \n");
+				acN = in.nextLine();
+				int i;
+				for (i = 0; i < count; i++) {
+					if (account[i].getAccountNo().equals(acN)) {
+						System.out.println("예금액을 입력해주세요.");
+						amount = Integer.parseInt(in.nextLine());
+						account[i].deposit(amount);
+						break;
+					}
+
+				}
+				if (i == count) {
+					System.out.println("계좌번호를 확인해주세요.");
+				}
+
+			}
+			case "4" -> { // 출금
+				System.out.println("출금 처리중... \n");
+				System.out.println("계좌번호를 입력해주세요. \n");
+				acN = in.nextLine();
+				int i;
+				for (i = 0; i < count; i++) {
+					if (account[i].getAccountNo().equals(acN)) {
+						System.out.println("출금액을 입력해주세요.");
+						amount = Integer.parseInt(in.nextLine());
+						account[i].withdraw(amount);
+						break;
+					}
+				}
+				if (i == count) {
+					System.out.println("계좌번호를 확인해주세요.");
+				}
+			}
 			default -> System.out.println("메뉴 확인 바랍니다.");
 			}
 		}
 		System.out.println("프로그램 종료");
 
-	}
-
-	static void createAccount() {
-		System.out.println("계좌 생성 처리 중...");
-	}
-
-	static void accountInquiry() {
-		System.out.println("계좌 목록 처리 중...");
-	}
-
-	static void depositProcess() {
-		System.out.println("예금 처리 중...");
-	}
-
-	static void withdrawProcess() {
-		System.out.println("출금 처리 중...");
 	}
 
 }
@@ -81,13 +125,20 @@ class Account {
 	public void deposit(int money) {
 		// overflow 고려해서 refactoring 할것 ==> 검증 필
 		long total = balance + money;
-		if (total > 2_000_000_000)
+		if (total < 2_000_000_000) {
 			balance = balance + money;
+		} else {
+			System.out.println("계좌 한도를 초과하였습니다.");
+		}
 	}
 
 	public void withdraw(int money) {
-		if (balance >= money)
+		if (balance >= money) {
 			balance = balance - money;
+			System.out.println("결과 : 출금이 성공되었습니다.");
+		} else {
+			System.out.println("계좌 잔액이 부족합니다.");
+		}
 	}
 
 	public int inquiry() {
--- src/exercise/DiceDemo2.java
+++ src/exercise/DiceDemo2.java
@@ -6,12 +6,25 @@ public class DiceDemo2 {
 
 	public static void main(String[] args) {
 		Scanner in = new Scanner(System.in);
+		int cycle = 0;
+		Dice2 dice2 = new Dice2();
+
+		System.out.println("주사위를 던질 횟수를 입력하세요.");
+		cycle = in.nextInt();
+
+		if (cycle < 1) {
+			System.out.println("자연수로만 입력해주세요.");
+		} else {
+			for (int i = 0; i < cycle; i++) {
+				dice2.roll();
+			}
+		}
 
 	}
 
 }
 
-class Dice {
+class Dice2 {
 	public void roll() {
 		System.out.println((int) (Math.random() * 6) + 1);
 	}
