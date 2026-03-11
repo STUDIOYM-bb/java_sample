2a1f6fbcf5a97f3ab72af4ea95dea4c813a9fde5
studioym-bb <studioym.bb@gmail.com>
2026-03-12 08:43:50 +0900

    day 11 lllast update

--- src/nakseo/Exercise2.java
+++ src/nakseo/Exercise2.java
@@ -1,16 +1,24 @@
 package nakseo;
 
+import java.util.Scanner;
+
 public class Exercise2 {
 
 	public static void main(String[] args) {
-
-		getnum(10);
-		getnum(20.2);
-		getnum(5.3f);
-		getnum("string");
-	}
-
-	public static <T extends Number> void getnum(T num) {
-		System.out.println(num);
+		Scanner sc = new Scanner(System.in);
+		System.out.println("입력하세요.");
+		int x = sc.nextInt();
+		int y = 0;
+		int count = 0;
+		for (int i = 2; i < x; i++) {
+			if ((x % i) == 0) {
+				y++;
+			}
+		}
+		if (y == 0) {
+			System.out.println("소수입니다.");
+		} else {
+			System.out.println("소수가 아닙니다.");
+		}
 	}
 }
--- src/nakseo/ThisIsJava.java
+++ src/nakseo/ThisIsJava.java
@@ -1,21 +1,27 @@
 package nakseo;
 
+import java.util.Scanner;
+
 public class ThisIsJava {
 
 	public static void main(String[] args) {
-		int e1 = 0;
-		int e2 = 0;
-		while (true) {
-			e1 = (int) (6 * Math.random() + 1);
-			e2 = (int) (6 * Math.random() + 1);
-			System.out.printf("( %d , %d )\n", e1, e2);
-
-			if (e1 + e2 == 5) {
-				break;
-			} else {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("입력하세요.");
+		int x = sc.nextInt();
+		int y = 0;
+		int count = 0;
+		for (int i = 2; i <= x; i++) {
+			for (int j = 2; j < i; j++) {
+				if (i % j == 0) {
+					y++;
+				}
 			}
+			if (y == 0) {
+				count++;
+			}
+			y = 0;
 
 		}
+		System.out.printf("1 부터 %d 까지 %d 개의 소수가 있습니다.", x, count);
 	}
-
 }
